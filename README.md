9cb99f8abadc4c693b014e71d8df4949eaa668a2
alexhe <alexhe2011@gmail.com>
2023-07-01 11:29:07 +0800

    python3 parser for mysql 8.0 innodb file

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # py3_innodb_page_info
-py3_innodb_page_info
+
+## 来源
+forked from https://github.com/happieme/py_innodb_page_info
+
+the old code can only supported by python2;
+
+current code supported python 3.10
+
+python tool for innodb page info   
+
+原来仓库代码很久没更新了，只支持python2。
+本代码基于现在主流的python3（我环境是python 3.10)修改而成。
+如果有其它语法不支持，也是少数，简单修改即可。
+
+针对MySQL 8.0改造，新增了不支持的类型:
+	# mysql 8.0: storage/innobase/include/fil0fil.h:1184
+	'45be':u'R-tree Node',
+	'45bd':u'Tablespace SDI Index page'
+
+
+## 用法
+首先 要安装python
+
+用法:
+
+python py3_innodb_page_info.py xxx.ibd -v
+
+- v 表示详细信息
+
+```
+$ python py3_innodb_page_info.py xxx.ibd -v
+page_type =  0008
+page offset 00000000, page type <File Space Header>
+page_type =  0005
+page offset 00000001, page type <Insert Buffer Bitmap>
+page_type =  0003
+page offset 00000002, page type <File Segment inode>
+page_type =  45bd
+page offset 00000003, page type <Tablespace SDI Index page>
+page_type =  45bf
+page offset 00000004, page type <B-tree Node>, page level <0000>
+page_type =  45bf
+page offset 00000005, page type <B-tree Node>, page level <0000>
+page_type =  0000
+page offset 00000000, page type <Freshly Allocated Page>
+page_type =  0000
+page offset 00000000, page type <Freshly Allocated Page>
+Total number of page: 8:
+File Space Header: 1
+Insert Buffer Bitmap: 1
+File Segment inode: 1
+Tablespace SDI Index page: 1
+B-tree Node: 2
+Freshly Allocated Page: 2
+```
---
+++ py3_innodb_page_info.py
@@ -0,0 +1,146 @@
+#! /usr/bin/env python 
+#encoding=utf-8
+'''
+forked from https://github.com/happieme/py_innodb_page_info
+the old code can only supported by python2;
+current code verified on python 3.10
+
+'''
+
+import os
+from sys import argv
+
+
+#encoding=utf-8
+INNODB_PAGE_SIZE = 16*1024*1024
+
+# Start of the data on the page
+FIL_PAGE_DATA = 38
+
+
+FIL_PAGE_OFFSET = 4 # page offset inside space
+FIL_PAGE_TYPE = 24 # File page type
+
+# Types of an undo log segment */
+TRX_UNDO_INSERT = 1
+TRX_UNDO_UPDATE = 2
+
+# On a page of any file segment, data may be put starting from this offset
+FSEG_PAGE_DATA = FIL_PAGE_DATA
+
+# The offset of the undo log page header on pages of the undo log
+TRX_UNDO_PAGE_HDR = FSEG_PAGE_DATA
+
+PAGE_LEVEL = 26	#level of the node in an index tree; the leaf level is the level 0 */
+				
+innodb_page_type={
+	'0000':u'Freshly Allocated Page',
+	'0002':u'Undo Log Page',
+	'0003':u'File Segment inode',
+	'0004':u'Insert Buffer Free List',
+	'0005':u'Insert Buffer Bitmap',
+	'0006':u'System Page',
+	'0007':u'Transaction system Page',
+	'0008':u'File Space Header',
+	'0009':u'extend description page',
+	'000a':u'Uncompressed BLOB Page',
+	'000b':u'1st compressed BLOB Page',
+	'000c':u'Subsequent compressed BLOB Page',
+	'45bf':u'B-tree Node',
+	# mysql 8.0: storage/innobase/include/fil0fil.h:1184
+	'45be':u'R-tree Node',
+	'45bd':u'Tablespace SDI Index page'
+	}
+	
+innodb_page_direction={
+	'0000': 'Unknown(0x0000)',
+	'0001': 'Page Left',
+	'0002': 'Page Right',
+	'0003': 'Page Same Rec',
+	'0004': 'Page Same Page',
+	'0005': 'Page No Direction',
+	'ffff': 'Unkown2(0xffff)'
+}
+
+
+INNODB_PAGE_SIZE=1024*16 # InnoDB Page 16K
+
+
+
+
+TABLESPACE_NAME='D:\\mysql_data\\test\\t.ibd'
+VARIABLE_FIELD_COUNT = 1
+NULL_FIELD_COUNT = 0
+
+class myargv(object):
+	def __init__(self, argv):
+		self.argv = argv
+		self.parms = {}
+		self.tablespace = ''
+	def usage(self):
+		print('Get InnoDB Page Info')
+		print('Usage: python py_innodb_page_info.py [OPTIONS] tablespace_file\n')
+		print('The following options may be given as the first argument:')
+		print('-h        help ')
+		print('-o output put the result to file')
+		print('-t number thread to anayle the tablespace file')
+		print('-v        verbose mode')
+	
+	def parse_cmdline(self):
+		argv = self.argv
+		if len(argv) == 1:
+			self.usage()
+			return 0 
+		while argv:
+			if argv[0][0] == '-':
+				if argv[0][1] == 'h':
+					self.parms[argv[0]] = ''
+					argv = argv[1:]
+					break
+				if argv[0][1] == 'v':
+					self.parms[argv[0]] = ''
+					argv = argv[1:]			
+				else:
+					self.parms[argv[0]] = argv[1]
+					argv = argv[2:]
+			else:
+				self.tablespace = argv[0]
+				argv = argv[1:]
+		if '-h' in self.parms:
+			self.usage()
+			return 0
+		return 1
+		
+def mach_read_from_n(page,start_offset,length):
+	ret = page[start_offset:start_offset+length]
+	return ret.hex()
+	
+def get_innodb_page_type(myargv):
+	f=open(myargv.tablespace,'rb')
+	fsize = int(os.path.getsize(f.name)/INNODB_PAGE_SIZE)
+	ret = {}
+	for i in range(fsize):
+		page = f.read(INNODB_PAGE_SIZE)
+		page_offset = mach_read_from_n(page,FIL_PAGE_OFFSET,4)
+		page_type = mach_read_from_n(page,FIL_PAGE_TYPE,2)
+		print("page_type = ", page_type)
+		if '-v' in myargv.parms:
+			if page_type == '45bf':
+				page_level = mach_read_from_n(page,FIL_PAGE_DATA+PAGE_LEVEL,2)
+				print("page offset %s, page type <%s>, page level <%s>"%(page_offset,innodb_page_type[page_type],page_level))
+			else:
+				print("page offset %s, page type <%s>"%(page_offset,innodb_page_type[page_type]))
+		if page_type not in ret:
+			ret[page_type] = 1
+		else:
+			ret[page_type] = ret[page_type] + 1
+	print("Total number of page: %d:"%fsize)
+	for type in ret:
+		print("%s: %s"%(innodb_page_type[type],ret[type]))
+
+if __name__ == '__main__':
+	myargv = myargv(argv)
+	if myargv.parse_cmdline() == 0:
+		pass
+	else:
+		get_innodb_page_type(myargv)
